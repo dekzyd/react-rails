471cd412d8e3e72c2b1e360349316c1d4d21deb1
dekzyd <adekoyadayo2010@gmail.com>
2023-07-31 14:05:34 +0100

    fix routing issue

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  get '/*path' => 'pages#home'
   # Defines the root path route ("/")
   # root "articles#index"
 end
