d9366da19a72320ed1d83f94f9efcc214b1abb2b
dekzyd <adekoyadayo2010@gmail.com>
2023-07-31 14:17:39 +0100

    sidenav works

--- app/javascript/components/HomeLayout.jsx
+++ app/javascript/components/HomeLayout.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Sidebar from "./Sidebar";
 
 const HomeLayout = () => {
   return (
     <>
       <h3>HomeLayout</h3>
+      <Sidebar />
       <Outlet />
     </>
   );
